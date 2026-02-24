7ce10165f79d18379199ee7b19b28ac82566a067
Bacsi József <bjozsef06@szikszi.local>
2026-02-24 13:28:45 +0100

    proba komment

--- obj/Debug/net9.0/GameStore.AssemblyInfo.cs
+++ obj/Debug/net9.0/GameStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc2d9d936d67faf21a9780590ccb2cc891ac6067")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
